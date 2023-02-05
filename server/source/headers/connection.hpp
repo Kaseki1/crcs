1819af5fc6f5ab70f96d27b3bcc7fd5f296672c9
Kaseki1 <kaseki1@protonmail.com>
2023-02-05 19:31:01 +0530

    Disabled SSL

--- server/source/headers/connection.hpp
+++ server/source/headers/connection.hpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
+//#include <openssl/ssl.h>
+//#include <openssl/err.h>
 #include <deque>
 #include <poll.h>
 #include "database.hpp"
@@ -17,42 +17,42 @@ namespace crcs
     {
     protected:
         int sock;
-        SSL* ssl;
+        //SSL* ssl;
         database conn_db;
         std::string session_id;
         std::deque<std::string> message_queue;
         
-        virtual SSL_CTX *create_context()
-        {
-            const SSL_METHOD *method;
-            SSL_CTX *ctx;
+        //virtual SSL_CTX *create_context()
+        //{
+        //    const SSL_METHOD *method;
+        //    SSL_CTX *ctx;
 
-            method = TLS_server_method();
+        //    method = TLS_server_method();
 
-            ctx = SSL_CTX_new(method);
-            if (!ctx) {
-                perror("Unable to create SSL context");
-                ERR_print_errors_fp(stderr);
-                exit(EXIT_FAILURE);
-            }
+        //    ctx = SSL_CTX_new(method);
+        //    if (!ctx) {
+        //        perror("Unable to create SSL context");
+        //        ERR_print_errors_fp(stderr);
+        //        exit(EXIT_FAILURE);
+        //    }
 
-            return ctx;
-        }
+        //    return ctx;
+        //}
         
-        virtual void configure_context(SSL_CTX *ctx)
-        {
-            /* Set the key and cert */
-            if (SSL_CTX_use_certificate_file(ctx, "/etc/crcs/ssl/cert.pem", SSL_FILETYPE_PEM) <= 0)
-            {
-                ERR_print_errors_fp(stderr);
-                exit(EXIT_FAILURE);
-                if (SSL_CTX_use_PrivateKey_file(ctx, "/etc/crcs/ssl/key.pem", SSL_FILETYPE_PEM) <= 0 )
-                {
-                    ERR_print_errors_fp(stderr);
-                    exit(EXIT_FAILURE);
-                }
-            }
-        }
+        //virtual void configure_context(SSL_CTX *ctx)
+        //{
+        //    /* Set the key and cert */
+        //    if (SSL_CTX_use_certificate_file(ctx, "/etc/crcs/ssl/cert.pem", SSL_FILETYPE_PEM) <= 0)
+        //    {
+        //        ERR_print_errors_fp(stderr);
+        //        exit(EXIT_FAILURE);
+        //        if (SSL_CTX_use_PrivateKey_file(ctx, "/etc/crcs/ssl/key.pem", SSL_FILETYPE_PEM) <= 0 )
+        //        {
+        //            ERR_print_errors_fp(stderr);
+        //            exit(EXIT_FAILURE);
+        //        }
+        //    }
+        //}
     public:
         connection(int s, std::string db_host, std::string db_user,
                    std::string db_passwd, std::string db_name,
